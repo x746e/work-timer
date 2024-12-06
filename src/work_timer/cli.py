a8a299191eb0d100a817cbe13cbd6f6aa3a2f8fe
Kirill Spitsyn <ksp@google.com>
2024-12-06 15:16:21 -0800

    A few improvements to the interactive cli:
    - Merge the logs with the tasks.
    - Remove microseconds from everywhere.
    - Add a ipython formater for DataFrames that prints timedeltas in a compact way.

--- src/work_timer/cli.py
+++ src/work_timer/cli.py
@@ -3,28 +3,71 @@
 from datetime import date, datetime, timedelta
 from pathlib import Path
 
+from traitlets.config import Config
 from IPython import embed
 import pandas as pd
 
 from work_timer import taskdb
 from work_timer import timelog
+from work_timer.utils.time import humanize_td
 
 
 def console():
     """Pull in tasks and time log DataFrames, and switch to an IPython console."""
-    # pylint: disable=unused-variable
-    task_db = taskdb.PersistentTaskDB(Path('~/dev-tasks/'))
-    time_log = timelog.PersistentTimeLog(Path('~/dev-timelog.json'))
-    # task_db = taskdb.PersistentTaskDB(Path('~/tasks/'))
-    # time_log = timelog.PersistentTimeLog(Path('~/timelog.json'))
+    # pylint: disable=unused-variable,possibly-unused-variable
+
+    # TODO: Select the DBs according to a config.
+    # task_db = taskdb.PersistentTaskDB(Path('~/dev-tasks/'))
+    # time_log = timelog.PersistentTimeLog(Path('~/dev-timelog.json'))
+
+    task_db = taskdb.PersistentTaskDB(Path('~/tasks/'))
+    time_log = timelog.PersistentTimeLog(Path('~/timelog.json'))
+
     tasks = task_db.get_data_frame()
     logs = time_log.get_data_frame()
+    logs = logs.merge(tasks, left_on='task_id', right_on='id', how='left')
+    # Drop microseconds.  TODO: Do it somewhere in the DBs instead.
+    logs.start = logs.start.apply(lambda dt: dt.replace(microsecond=0))
+    logs.duration = logs.duration.apply(lambda td: pd.Timedelta(seconds=int(td.total_seconds())))
     # Today logs.
     tlogs = logs[logs.start.dt.date == date.today()]
+    tlogs = tlogs.drop(columns=['description'])
+    tlogs.start = tlogs.start.apply(lambda dt: dt.time())
     twork = tlogs[tlogs.task_id != -2]
     tbreaks = tlogs[tlogs.task_id == -2]
 
-    embed()
+    # Print out what objects are available.
+    print(f'Variables available: {list(sorted(locals()))}')
+
+    pd.set_option('display.width', 0)
+    pd.set_option('display.max_columns', 10)
+
+    # TODO: Maybe print out some overall stats for today.
+
+    # TODO: Pull the current period as well if the timer is running.
+
+    # TODO: A `refresh()` function to pull new data.
+    # TODO: Watch the DB changes, either just refresh automatically, or print about
+    #       it in the console.
+
+    # For reference, inside ipython that's the way to change the formatters:
+    # ip = get_ipython()
+    # formatter = ip.display_formatter.formatters['text/plain']
+    # formatter.for_type(pd.DataFrame, data_frame_formatter)
+    c = Config()
+    c.PlainTextFormatter.type_printers = {pd.DataFrame: data_frame_formatter}
+    embed(config=c)
+
+
+def data_frame_formatter(df, p, cycle):
+    assert not cycle
+    timedelta_columns = [
+            column
+            for column, type_ in zip(df.columns, df.dtypes)
+            if pd.api.types.is_timedelta64_dtype(type_)]
+    df = df.assign(**{column: df[column].apply(humanize_td)
+                      for column in timedelta_columns})
+    p.text(str(df))
 
 
 def main():
--- src/work_timer/utils/time.py
+++ src/work_timer/utils/time.py
@@ -7,6 +7,7 @@ def td(s: str | timedelta) -> timedelta:
     """𝑇ime 𝐷elta.
 
     Currently can parse "<int>(s|m|h)", like "10h" or "15m" or "3s".
+    For example:
 
     >>> td('5m')
     datetime.timedelta(seconds=300)
@@ -15,20 +16,58 @@ def td(s: str | timedelta) -> timedelta:
     >>> td('1h5m10s')
     datetime.timedelta(seconds=3910)
 
+    It also returns `timedelta` as is:
+
     >>> td(timedelta(seconds=36000))
     datetime.timedelta(seconds=36000)
     """
     if isinstance(s, timedelta):
         return s
 
-    m = re.match(r'((?P<hours>\d+)h)?'
+    m = re.match(r'((?P<days>\d+)d)?'
+                 r'((?P<hours>\d+)h)?'
                  r'((?P<minutes>\d+)m)?'
                  r'((?P<seconds>\d+)s)?$', s)
     if not m:
         raise ValueError(f"Can't parse {s!r}")
 
     groupdict = m.groupdict(default=0)
+    days = int(groupdict['days'])
     hours = int(groupdict['hours'])
     minutes = int(groupdict['minutes'])
     seconds = int(groupdict['seconds'])
-    return timedelta(seconds=((int(hours) * 60) + int(minutes)) * 60 + int(seconds))
+    return timedelta(
+        seconds=(((int(days) * 24 + int(hours)) * 60) + int(minutes)) * 60 + int(seconds)
+    )
+
+
+def humanize_td(delta: timedelta) -> str:
+    """Convert timedelta into a compact string.
+
+    A reverse operation to `td` above.
+
+    >>> humanize_td(td('5m'))
+    '5m'
+    >>> humanize_td(td('5d5h5m5s'))
+    '5d5h5m5s'
+    >>> humanize_td(timedelta(hours=5, microseconds=42))
+    '5h42μs'
+    """
+    microseconds = delta.microseconds
+    seconds = int(delta.total_seconds())
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+
+    parts = []
+    if days:
+        parts.append(f'{days}d')
+    if hours:
+        parts.append(f'{hours}h')
+    if minutes:
+        parts.append(f'{minutes}m')
+    if seconds:
+        parts.append(f'{seconds}s')
+    if microseconds:
+        parts.append(f'{microseconds}μs')
+    return ''.join(parts)
