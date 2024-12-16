19075b5c471578d4fb3b086b06d3e766419b5b96
Kirill Spitsyn <ksp@google.com>
2024-12-15 21:41:47 -0800

    Rename timer.Timer into _SingleTaskTimer, add a new Timer.

--- src/work_timer/timer.py
+++ src/work_timer/timer.py
@@ -22,7 +22,7 @@ from work_timer.utils.clock import Clock
 Seconds = NewType('Seconds', int)
 
 
-class Timer(state_machine.StateMachine):
+class _SingleTaskTimer(state_machine.StateMachine):
     """The timer."""
 
     # TODO: Try removing it, now with _TimeKeeper we can we OK.
@@ -130,6 +130,44 @@ class Timer(state_machine.StateMachine):
         if self._evt_id:
             self._scheduler.cancel(self._evt_id)
             self._evt_id = None
+
+
+class Timer:
+
+    """The timer.
+
+    For now just forwards everything to the underlying _SingleTaskTimer.  The
+    plan is to move all the timing logic from ui.timer into this class.
+    """
+
+    State = _SingleTaskTimer.State
+
+    def __init__(
+            self,
+            task_id: TaskID,
+            period_length: timedelta,
+            time_log: timelog.TimeLog,
+            clock: Clock = time):
+        self._single_task_timer = _SingleTaskTimer(
+            task_id, period_length, time_log, clock)
+
+    def start(self):
+        self._single_task_timer.start()
+
+    def stop(self):
+        self._single_task_timer.stop()
+
+    def pause(self):
+        self._single_task_timer.pause()
+
+    def resume(self):
+        self._single_task_timer.resume()
+
+    def get_info(self) -> 'TimerInfo':
+        return self._single_task_timer.get_info()
+
+    def set_on_period_end_callback(self, callback: Callable[['TimerInfo'], None]):
+        self._single_task_timer.set_on_period_end_callback(callback)
 
 
 @dataclass(frozen=True)
