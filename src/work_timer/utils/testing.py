3c532f5c6d41f0ef6fdcbd8a574f45eab7af19bd
Kirill Spitsyn <ksp@google.com>
2024-12-18 14:04:59 -0800

    Trying to make FakeClock to work better with threads.

--- src/work_timer/utils/testing.py
+++ src/work_timer/utils/testing.py
@@ -1,6 +1,7 @@
 """Misc testing helpers."""
 from datetime import timedelta
 import time
+from threading import Condition
 from typing import Protocol, Callable
 
 from work_timer.utils import clock
@@ -19,12 +20,19 @@ class FakeClock(clock.Clock):
     actually waiting for time to pass.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._time = 0.
         self._stopped = False
+        self._future_is_now = Condition()
 
-    def advance(self, delta: timedelta | str, ticks=30):
+    def advance(self, delta: timedelta | str, ticks=30) -> None:
         """Move the fake time by `delta`."""
+
+        def time_travel(to: float) -> None:
+            with self._future_is_now:
+                self._time = to
+                self._future_is_now.notify_all()
+
         delta_sec = td(delta).total_seconds()
         # `(float_num / ticks) * ticks` isn't necessarily equals `float_num`,
         # so let's calculate what the time should be at the end, so set this
@@ -32,22 +40,27 @@ class FakeClock(clock.Clock):
         advance_to = self._time + delta_sec
         inc = delta_sec / ticks
         for _ in range(ticks):
-            self._time += inc
+            time_travel(to=self._time + inc)
             time.sleep(0)
-        if self._time < advance_to:  # pylint: disable=consider-using-max-builtin
-            self._time = advance_to
+
+        if self._time < advance_to:
+            time_travel(advance_to)
+
         time.sleep(0.001)
 
     def time(self) -> float:
         return self._time
 
-    def sleep(self, seconds: float, /):
+    def sleep(self, seconds: float, /) -> None:
         until = self._time + seconds
-        while self._time < until and not self._stopped:
-            pass
+        with self._future_is_now:
+            while self._time < until and not self._stopped:
+                self._future_is_now.wait()
 
-    def stop(self):
+    def stop(self) -> None:
         # Move forward for a bit, to allow callbacks to fire.
         self._stopped = True
         self.advance('1h')
         self._time = 2**32
+        with self._future_is_now:
+            self._future_is_now.notify_all()
